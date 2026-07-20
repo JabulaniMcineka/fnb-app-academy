c5570d67305975d407b96e4d8224e1f409e1de04
JabulaniMcineka <Jabulani.Mcineka@ahri.org>
2026-07-20 13:59:37 +0200

    Add Python lesson files

---
+++ lesson6.py
@@ -0,0 +1,29 @@
+# Workin with lists (Shopping cart)
+
+
+
+print ("First")
+cart = ["apple", "banana", "orange", "grapes"]
+print(cart[0])
+
+print ("second")
+cart.append("Bread")
+print(cart)
+
+
+
+print ("Third")
+for item in cart:
+    print(item)
+
+
+
+# working with dictionaries
+
+student = {
+    "name": "Thabo",
+    "age": 20,
+    "course": "Intro to Python"
+}  
+
+print(student["name"])
